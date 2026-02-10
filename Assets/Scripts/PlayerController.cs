0c14b4ddcacd7cb702cf9e3e0b58c6d5169aea73
Cristhian Camilo Preciado Lema <l.lema97@gmail.com>
2026-02-10 15:35:44 -0500

    Initial commit: Unity Fishing Game MVP setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // 👇 ESTA es la firma correcta para "Send Messages"
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
